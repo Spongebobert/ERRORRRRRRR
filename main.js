3e4fe51572a3b6e786b7f8bdf89e8ad76614097c
Spongebobert <107082372+Spongebobert@users.noreply.github.com>
2022-10-22 10:18:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -6,15 +6,15 @@ ball_y=0;
 ball_x=0;
 hole_y=400;
 hole_x=800;
+
 block_image_width = 5;
 block_image_height = 5;
 
-function load_img(){
+function loadimg(){
 	fabric.Image.fromURL("golf-h1.png", function(Img)  {
-	hole_obj.onload
 	hole_obj = Img;
 	hole_obj.scaleToWidth(50);
-	hole_obj.scaleToWidth(50);
+	hole_obj.scaleToHeight(50);
 	hole_obj.set({
 		top:hole_y,
 		left:hole_x
@@ -46,13 +46,10 @@ function my_keydown(e)
 	console.log(keyPressed);
 	if((ball_x==hole_x)&&(ball_y==hole_y)){
 		canvas.remove(ball_obj);
+		console.log("You have hit the goal!!!");
+		document.getElementById("hd3").innerHTML="You have Hit the Goal!!!"
+		document.getElementById("myCanvas").style.borderColer="red";
 	}
-
-	/* Check the coordinates of the ball and hole images to finish the game. 
-	And id coordinates matches them remove ball image, 
-	display "GAME OVER!!!" 
-	and make canvas border 'red'. */
-	
 	else{
 		if(keyPressed == '38')
 		{
@@ -78,7 +75,7 @@ function my_keydown(e)
 	
 	function up()
 	{
-		if(ball_y <=0)
+		if(ball_y >=5)
 		{
 		ball_y = ball_y - block_image_height;
 			console.log("block image height = " + block_image_height);
@@ -105,7 +102,7 @@ function my_keydown(e)
 	{
 		if(ball_x >5)
 		{
-			ball_x = ball_x + block_image_width;
+			ball_x = ball_x - block_image_width;
 			console.log("block image width = " + block_image_width);
 			console.log("When Left arrow key is pressed x = " + ball_x +", Y = "+ball_y);
 			canvas.remove(ball_obj);
